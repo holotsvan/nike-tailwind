3dacd35ecd01ce5fc76e20457cd9a9816b15bb3a
Ivan Holotsvan <iholotsvan@gmail.com>
2024-01-29 19:31:46 +0200

    Add popular products section and hero seciton

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ label, iconURL }) => {
+  return (
+    <button className="flex justify-center items-center
+                       gap-2 px-7 py-4 border font-montserrat
+                       text-lg leading-none rounded-full
+                        text-white bg-coral-red border-coral-red">
+      {label}
+      <img src={iconURL} alt="arrow" className="ml-2 rounded-full w-5 h-5" />
+    </button>
+  );
+};
+
+export default Button;
